bc5f866016e33c819d8de9a374cd54e0df66a1a0
jmaff <jmaff2003@gmail.com>
2024-04-13 16:18:30 -0400

    Single PDF game generation working

--- api/gemini_poc.py
+++ api/gemini_poc.py
@@ -1,9 +1,10 @@
 import os
-import json
-import time
+from datetime import datetime
+from io import BytesIO
 
 import google.generativeai as genai
-from typing import List, Dict, Optional
+from typing import List, Any
+import fitz
 
 from prompts import *
 from data.text import *
@@ -11,13 +12,53 @@ from data.text import *
 genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 
 model = genai.GenerativeModel(model_name="models/gemini-1.5-pro-latest")
-game_content = [OS_1, OS_2, CV_1, CV_2]
+files_to_cleanup = []
 
 
-def generate_game():
+def generate_game(game_content):
     response = model.generate_content([JEOPARDY_GENERATE_GAME_PROMPT, *game_content])
     print(response.text)
 
 
+def extract_pdf_content(filename: str) -> List[Any]:
+    content = []
+    pdf_file = fitz.open(filename)
+
+    global_img_idx = 0
+    for page_index in range(len(pdf_file)):
+        page = pdf_file[page_index]
+        text = page.get_text()
+
+        content.append(text)
+
+        for img in page.get_images():
+            xref = img[0]
+
+            base_image = pdf_file.extract_image(xref)
+            image_bytes = base_image["image"]
+            image_ext = base_image["ext"]
+
+            tmp_filepath = (
+                f"tmp/{int(datetime.now().timestamp())}_{global_img_idx}.{image_ext}"
+            )
+
+            global_img_idx += 1
+            with open(tmp_filepath, "wb") as file:
+                file.write(BytesIO(image_bytes).getvalue())
+
+            upload_resp = genai.upload_file(path=tmp_filepath)
+            content.append(upload_resp)
+            files_to_cleanup.append(upload_resp)
+
+    return content
+
+
 if __name__ == "__main__":
-    print(generate_game())
+    content = extract_pdf_content("data/376complexitynotes.pdf")
+
+    print(len(content))
+
+    print(generate_game(content))
+
+    for f in files_to_cleanup:
+        genai.delete_file(f.name)
